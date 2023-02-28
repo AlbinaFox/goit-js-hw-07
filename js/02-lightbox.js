782145b7395f9ed22f69ada904f542b7da1db534
AlbinaFox <albina.kokoieva98@gmail.com>
2023-02-28 20:12:46 +0300

    update

---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+const galleryWrapperEl = document.querySelector(".gallery");
+
+
+const makeGallery = (galleryItems) => {
+  const arr = [];
+  galleryItems.map((el) => {
+      const galleryItem = `
+    <a class="gallery__item" href="${el.original}">
+    <img
+      class="gallery__image"
+      data-alt="${el.description}"
+      src="${el.preview}"
+      alt="${el.description}"
+    />
+    </a>
+   `;
+    arr.push(galleryItem);
+  });
+  const gallery = arr.join("");
+  galleryWrapperEl.innerHTML = gallery;
+};
+
+makeGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
+console.log(lightbox);
